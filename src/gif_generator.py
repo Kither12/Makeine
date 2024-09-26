a2608aee00df1dd1f004efeb471a341aaadabcb9
Kither12 <developerkither2509@gmail.com>
2024-09-26 12:31:37 +0700

    change base model for faster and more realistic gif generator

--- src/gif_generator.py
+++ src/gif_generator.py
@@ -1,5 +1,5 @@
 import torch
-from diffusers import AnimateDiffPipeline, DDIMScheduler, MotionAdapter
+from diffusers import AnimateDiffPipeline, DPMSolverMultistepScheduler, MotionAdapter
 from diffusers.utils import export_to_gif
 
 
@@ -11,13 +11,14 @@ class GifGenerator:
         adapter = MotionAdapter.from_pretrained(
             "guoyww/animatediff-motion-adapter-v1-5-2", torch_dtype=torch.float16)
         # load SD 1.5 based finetuned model
-        model_id = "SG161222/Realistic_Vision_V5.1_noVAE"
+        model_id = "emilianJR/epiCRealism"
         pipe = AnimateDiffPipeline.from_pretrained(
             model_id, motion_adapter=adapter, torch_dtype=torch.float16)
-        scheduler = DDIMScheduler.from_pretrained(
+        scheduler = DPMSolverMultistepScheduler.from_pretrained(
             model_id,
             subfolder="scheduler",
             clip_sample=False,
+            solver_order=3,
             timestep_spacing="linspace",
             beta_schedule="linear",
             steps_offset=1,
@@ -39,7 +40,7 @@ class GifGenerator:
             negative_prompt="lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature",
             num_frames=16,
             guidance_scale=7.5,
-            num_inference_steps=25,
+            num_inference_steps=20,
             generator=torch.Generator("cpu"),
         )
         frames = output.frames[0]
